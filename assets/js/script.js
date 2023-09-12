843cdd0ecb9484e3140ba9b72fb0f6e50d690f2b
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-12 17:54:46 +0200

    refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,31 +10,26 @@ const { createApp } = Vue
                title: 'Marvel\'s Spiderman Miles Morale',
                text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                alt: "Svezia",
-               id: 1
             },{
                image: './assets/img/02.webp',
                title: 'Ratchet & Clank: Rift Apart',
                text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
                alt: "Ratchet & Clank",
-               id: 2
             }, {
                image: './assets/img/03.webp',
                title: 'Fortnite',
                text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                alt: "Fortnite",
-               id: 3
             }, {
                image: './assets/img/04.webp',
                title: 'Stray',
                text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                alt: "Stray",
-               id: 4
             }, {
                image: './assets/img/05.webp',
                title: "Marvel's Avengers",
                text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                alt: "Avengers",
-               id: 5
             }
          ],
          activeImage: 0,
@@ -54,7 +49,7 @@ const { createApp } = Vue
          }
       },
       clickThumb(e){
-         this.activeImage = e.target.id - 1;
+         this.activeImage = e.currentTarget.id;
       },
       auto(){
          this.autoSlider = setInterval(this.next, 3000)
